827cae75faabc664598439b4e81240af79135460
pouyasadri <pouyapaghandeh78@gmail.com>
2024-02-18 22:35:23 +0100

    Initial Commit

---
+++ LampApp/LampAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampAppApp.swift
+//  LampApp
+//
+//  Created by Pouya Sadri on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LampAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
